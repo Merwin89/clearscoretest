0d811ae150f46465b1b5947691fa382a2ed791f7
Merwin89 <52861896+Merwin89@users.noreply.github.com>
2019-07-13 22:58:29 +0530

    Add files via upload

---
+++ ClearScore/src/clearscoretest/Signup.java
@@ -0,0 +1,84 @@
+package clearscoretest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+@Test
+public class Signup extends TestMain {
+
+		public void signuptest() throws InterruptedException
+		{
+    	Boolean signup_button = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/nav/div/ul/li[8]/a")).isDisplayed();
+    	
+    	Boolean	signup_button_enable = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/nav/div/ul/li[8]/a")).isEnabled();
+    	
+    	if ((signup_button == true) &&(signup_button_enable == true))
+    	{
+    		System.out.println("Sign up button is available and enabled to Click");
+    	}
+    	else 
+    	{
+    		System.out.println("Sign up button is unavailable");
+    	}
+    	
+    	driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/nav/div/ul/li[8]/a")).click();
+    	System.out.println("Clicked the Sign Up button");
+    	try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	String URL = driver.getCurrentUrl();
+    	AssertJUnit.assertEquals(URL, "https://app.clearscore.com/signup" );
+    	System.out.println("We are on the signup page since the url matches...yay!!");
+    	
+    	WebElement dropDown = driver.findElement( By.xpath( "//*[@id='signupform_region_input']" ) );
+    	List<WebElement> allOptions = dropDown.findElements(By.xpath(".//option"));
+    	//List<WebElement> elements = driver.findElements(By.xpath("//*[@id='signupform_region_input']"));
+        System.out.println("Number of elements:" +allOptions.size());
+        System.out.println("Verifying if the list contains the countries: India, United Kingdom and South Africa");
+        System.out.println("Verification on the way");
+        for (int i=0; i<allOptions.size();i++)
+        {
+          
+          List<String> valid = Arrays.asList("India", "United Kingdom", "South Africa");
+               
+          if (valid.contains(allOptions.get(i).getText()))
+          {
+        	  System.out.println(allOptions.get(i).getText());
+          }
+          
+        }
+        System.out.println("Hence verified");
+        
+         driver.findElement(By.xpath("//*[@id='signupform_email_input']")).sendKeys("test3@mailinator.com");
+         Boolean button_on = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div/form/div[3]/button")).isEnabled();
+        System.out.println("Is the Sign up button enabled? :"+" "+ button_on +" "+ "...since email id entered is valid" ); // right email id
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div/form/div[3]/button")).click();
+        
+		Thread.sleep(4000);
+		
+        URL = driver.getCurrentUrl();
+        
+    	if (URL.contains("https://app.clearscore.com/step1") == true)
+    	{
+    		System.out.println("We are in the the Step 1 of registration!!!");
+    	}
+    	else
+    	{
+    		System.out.println("Sorry bruv, you are on some other page i.e" + " " + URL);
+    	}
+}
+}
+
+    
+
+
+
---
+++ ClearScore/src/clearscoretest/TestMain.java
@@ -0,0 +1,131 @@
+package clearscoretest;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
+
+public class TestMain {
+
+	public static WebDriver driver; 
+	public static String vURL = "http://www.clearscore.com";;
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+ "\\Resources\\geckodriver.exe");
+
+		driver = new FirefoxDriver();
+		driver.get(vURL);
+		driver.manage().deleteAllCookies(); //Deleting all existing cookies from browser for a fresh start
+		System.out.println("Preparing test environment - Deleted all cookies");
+		
+		System.out.println("Test case 1 ==== Check if Notification is present");
+		
+		
+		Testcase1(); //Presence of notification is verified
+		
+		System.out.println("Test case 3 ==== The notification does not reappear after being dismissed");
+		Testcase1(); //Notification does not reappear verification
+		
+		Cookiewrite();	//Cookie setting verification
+		
+		
+		//Signup();  //Signup button functionality verification
+		
+		Signup signup = new Signup();
+		signup.signuptest();
+		
+		driver.close();
+		driver.quit();
+    			
+	}
+	
+	
+	@Test
+		public static void Testcase1()
+	{
+		
+		
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	//Boolean notification = driver.findElement(By.className("websiteCookieNotice--2LLwd")).isDisplayed();
+				
+		
+	try {
+		if(driver.findElement(By.className("websiteCookieNotice--2LLwd")) != null)
+		{
+			System.out.println("Cookie Notification is displayed?");
+			//System.out.println(notification);
+			
+			Boolean dismiss = driver.findElement(By.className("button---dYCq")).isDisplayed();
+			if(dismiss == true)
+			{
+				System.out.println("Test case 2 ==== Check if Notification can be dismissed");
+				System.out.println("The notification can be dismissed");
+				driver.findElement(By.className("button---dYCq")).click();			
+			}
+			
+		}
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		System.out.println("The Notification is no longer present");
+	}
+	}
+	
+	@Test
+	public static void Cookiewrite()
+	{
+		// create file named Cookies to store Login Information		
+        File file = new File("ClearScoreCookies.data");							
+        try		
+        {	  
+            // Delete old file if exists
+			file.delete();		
+            file.createNewFile();			
+            FileWriter fileWrite = new FileWriter(file);							
+            BufferedWriter Bwrite = new BufferedWriter(fileWrite);							
+            // loop for getting the cookie information 		
+            	
+            // loop for getting the cookie information 		
+            for(Cookie cookies : driver.manage().getCookies())							
+            {			
+                Bwrite.write((cookies.getName()+"  "+cookies.getValue()+"  "+cookies.getDomain()+"  "+cookies.getPath()+"  "+cookies.getExpiry()+"  "+cookies.isSecure()));																									
+                Bwrite.newLine();             
+            }			
+            Bwrite.close();			
+            fileWrite.close();
+            System.out.println("The cookies are saved in ClearScoreCookies.data");
+            
+        }
+        catch(Exception ex)					
+        {		
+            ex.printStackTrace();			
+        }		
+	
+        
+	}
+	
+
+	
+	
+	
+}
